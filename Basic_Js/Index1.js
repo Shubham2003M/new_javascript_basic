142e1700fad7e1ba82d0ec56d5fcf97f6e64d2bf
Shubham2003M <124388313+Shubham2003M@users.noreply.github.com>
2024-11-09 17:09:46 +0000

    dataTypes

---
+++ Basic_Js/Index1.js
@@ -0,0 +1,11 @@
+console.log("jai mata di")
+const myName="Shubham Mishra"
+let AccountId;
+/* 
+AccountId=23 // Not allowed
+Prefer not to Use Var 
+Because of Issue in Block Scope and functional Scope
+*/
+let myEmaill="Shubhammi2002@gmail.com"
+
+console.table([myName,myEmaill,AccountId])
---
+++ Basic_Js/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"// treat all Js Code as Newer version
+
+//alert(4+4)
+/*
+Numer=2 to power 53
+BigInt
+String
+Boolean
+null-=standalone Value
+Undefined
+Symbol= unique
+
+*/
+
+console.log(typeof "Shadow");
+console.log(typeof Undefined); //its Undefined
+console.log(typeof null); // its Object
+
